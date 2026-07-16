17d4a8151ebaa60932d4337b41b2d7f848b0f0b0
sergiocotiazure20265-ops <sergiocotiazure20265@gmail.com>
2026-07-15 21:55:57 -0300

    Tela de cadastro de produto e integração com API.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,39 @@
-import { Component, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, signal } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('web-produtos');
+
+  //Inicializando o HttpClient
+  private http = inject(HttpClient);
+
+  //Criando um JSON para capturar os campos do forulário
+  //e este JSON será utilizado para enviar os dados
+  //para a API.
+  produto = new FormGroup({
+    nome: new FormControl(''), //campo
+    preco: new FormControl('0'), //campo
+    quantidade: new FormControl('0'), //campo
+  });
+
+  //Função que será executada quando 
+  //o botão SUBMIT do formulário for clicado
+  cadastrarProduto() {
+    //Fazendo a chamada para o ENDPOINT POST da API (cadastrar produto)
+    this.http.post('http://localhost:8081/api/v1/produtos', this.produto.value)
+      .subscribe((resposta) => { //capturando o retorno da API
+          console.log(resposta); //imprimindo os dados da resposta no console
+      });
+  }
 }
